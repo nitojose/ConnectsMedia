fcd6057c1b6fbc4fb4953254ff7ae6059f5a878c
Liviyaks <50103578+Liviyaks@users.noreply.github.com>
2022-01-17 16:46:49 +0530

    html css code 1st commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './pages/headerSection'
+import About from './pages/aboutSection'
+import Features from './pages/features'
+
+function App() {
+  return (
+    <>
+    <div className='headerBanner'>
+      <Header />
+      <About />
+      <Features />
+
+    </div>
+      
+    </>
+  );
+}
+
+export default App;
